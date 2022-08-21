6411b7fcc9a1662ba9035d2ce150e3d185ec1e09
Glowman554 <glowman434@gmail.com>
2022-08-21 10:31:58 +0200

    change

--- server/command/permission.ts
+++ server/command/permission.ts
@@ -2,6 +2,6 @@ import { log } from "../logger.ts";
 
 export function check_permission(user: string, permission: string|undefined): boolean {
 	// TODO: implement permissions
-	log("WARNING", "Not implemented yet");
+	log("WARNING", "check_permission not implemented yet");
 	return !(permission == "blacklist");
 }

c7a2dbcaceafb5f6e1a65be2f947af05e95732a1
Glowman554 <glowman434@gmail.com>
2022-08-23 23:36:52 +0200

    oops

--- apps/server/utils/help.ts
+++ apps/server/utils/help.ts
@@ -1,5 +1,5 @@
 import { config } from "../config/config.ts";
 
 export function help_text(text: string) {
-	return text.replaceAll("<prefix>", config.get("prefix") as string).replaceAll("<bot_name>", "TheBot");
+	return text.replaceAll("<prefix>", config.get("command_prefix") as string).replaceAll("<bot_name>", "TheBot");
 }

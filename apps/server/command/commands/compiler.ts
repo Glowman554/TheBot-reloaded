d30a6e808ee9b2627c622e04eb34d7592655d7fa
Glowman554 <glowman434@gmail.com>
2022-08-23 23:33:26 +0200

    added help strings for compiler

--- apps/server/command/commands/compiler.ts
+++ apps/server/command/commands/compiler.ts
@@ -1,6 +1,7 @@
 import { compiler, CompilerResult, init_compiler_api } from "../../api/compiler.ts";
 import { config } from "../../config/config.ts";
 import { loadable } from "../../loadable.ts";
+import { help_text } from "../../utils/help.ts";
 import { Command, command_manager, CommandEvent, CommandExecutor, CommandResponse, empty, fail } from "../command.ts";
 import { check_permission } from "../permission.ts";
 
@@ -9,7 +10,7 @@ export default class Compiler implements loadable {
 		init_compiler_api(config.get("server", "compiler") as string);
 
 		command_manager.add_command(
-			new Command("run", "", "", {
+			new Command("run", "Run a command!", help_text("Use '<prefix>run [command?]' to run the command or the quoted text!"), {
 				execute: async (event: CommandEvent): Promise<CommandResponse> => {
 					if (!event._1_arg_or_quote_text()) {
 						return fail;
@@ -31,7 +32,7 @@ export default class Compiler implements loadable {
 		);
 
 		command_manager.add_command(
-			new Command("compile", "", "", {
+			new Command("compile", "Compile and run a file!", help_text("Use '<prefix>compile' to compile and run the attached or quoted file!"), {
 				execute: async (event: CommandEvent): Promise<CommandResponse> => {
 					if (event.interface.args.length != 0 || event.interface.files == undefined || event.interface.files.length == 0) {
 						return fail;

986accc1d555f1e77995b5908a95348611a80754
Glowman554 <glowman434@gmail.com>
2022-08-24 22:09:26 +0200

    added coinflip command

--- apps/server/command/commands/fun.ts
+++ apps/server/command/commands/fun.ts
@@ -1,7 +1,22 @@
 import { loadable } from "../../loadable.ts";
+import { help_text } from "../../utils/help.ts";
 import { Command, command_manager, CommandEvent, CommandExecutor, CommandResponse, empty, fail } from "../command.ts";
 
 export default class Fun implements loadable {
 	load(): void {
+		command_manager.add_command(
+			new Command("coinflip", "Flip a coin!", help_text("Use '<prefix>coinflip' to flip a coin!"), {
+				execute: async (event: CommandEvent): Promise<CommandResponse> => {
+					if (event.interface.args.length != 0) {
+						return fail;
+					}
+
+					return {
+						is_response: true,
+						response: Math.random() >= 0.5 ? "You've landed on heads!" : "You've landed on tails!"
+					};
+				},
+			} as CommandExecutor, undefined),
+		);
 	}
 }

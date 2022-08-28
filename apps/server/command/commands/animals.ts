047043f22ee52c2c7ab7e6dd7f44b1943744d73e
Glowman554 <glowman434@gmail.com>
2022-08-28 20:48:41 +0000

    added furry command

--- apps/server/command/commands/animals.ts
+++ apps/server/command/commands/animals.ts
@@ -4,6 +4,7 @@ import { Command, command_manager, CommandEvent, CommandExecutor, CommandRespons
 import { get_cat, get_dog, get_fox } from "../../api/animals.ts";
 import { download_to_tmp_file } from "../../api/download.ts";
 import { help_text } from "../../utils/help.ts";
+import { FurryApi } from "../../api/furry.ts";
 
 export default class Animals implements loadable {
 	load(): void {
@@ -47,6 +48,33 @@ export default class Animals implements loadable {
 					}
 
 					var file = await download_to_tmp_file(await get_cat());
+
+					await event.interface.send_picture_message(file);
+
+					return empty;
+				},
+			} as CommandExecutor, undefined),
+		);
+
+		command_manager.add_command(
+			new Command("furry", "See a cute furry image!", help_text("Use '<prefix>furry [method?/list?]' to see a cute furry image!"), {
+				execute: async (event: CommandEvent): Promise<CommandResponse> => {
+					if (event.interface.args.length != 1 && event.interface.args.length != 0) {
+						return fail;
+					}
+
+					var method = "furry/fursuit";
+					if (event.interface.args.length == 1) {
+						var method = event.interface.args[0];
+						if (method == "list") {
+							return {
+								is_response: true,
+								response: "I know: " + new FurryApi()._methods.join(", ")
+							};
+						}
+					}
+
+					var file = await download_to_tmp_file(await new FurryApi().methods[method]());
 
 					await event.interface.send_picture_message(file);
 

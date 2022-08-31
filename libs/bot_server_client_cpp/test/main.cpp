6b1bc89f3b46d780f5788bae5172cd24412977a7
Glowman554 <glowman434@gmail.com>
2022-08-31 06:15:22 +0000

    make example on how to make custom_connection

--- libs/bot_server_client_cpp/test/main.cpp
+++ libs/bot_server_client_cpp/test/main.cpp
@@ -3,10 +3,25 @@
 #include <protocol.h>
 #include <socket.h>
 
+class custom_connection : public connection {
+	public:
+		custom_connection(client* c) : connection(c) {}
+
+		virtual void on_auth(protocol::key_auth_response pkg) override {
+			std::cout << "In my custom auth (" << pkg.success << ")" << std::endl;
+		}
+
+		virtual void on_open(client* c, websocketpp::connection_hdl hdl) override {
+			std::cout << "In custom on_open" << std::endl;
+
+			connection::on_open(c, hdl); // calling the super function here is VERY IMPORTANT
+		}
+};
+
 int main() {
 	websocket_endpoint endp;
 
-	auto con = new connection(endp.get_client());
+	auto con = new custom_connection(endp.get_client());
 	int id = endp.connect("ws://172.24.1.87:8080", con);
 
 	con->authenticate("thebestbot");
@@ -14,7 +29,7 @@ int main() {
 
 	std::cout << con->config_helper.get("root", "command_prefix", con).dump() << std::endl;
 	std::cout << con->tmp_helper.get("txt", 10, con) << std::endl;
-	
+
 	con->message("owo!ping", "cpp", "cpp", std::vector<std::string>{}, "", std::vector<std::string>{}, 10);
 
 	auto sock = endp.get(id);

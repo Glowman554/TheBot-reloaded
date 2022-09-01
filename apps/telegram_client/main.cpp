3eb3c50be0cc55887dc078f59fa1c2006a6e125a
Glowman554 <glowman434@gmail.com>
2022-09-01 20:59:03 +0200

    load connection deltails from file

--- apps/telegram_client/main.cpp
+++ apps/telegram_client/main.cpp
@@ -3,6 +3,8 @@
 #include <protocol.h>
 #include <socket.h>
 #include <tgbot/tgbot.h>
+#include <fstream>
+#include <nlohmann/json.hpp>
 
 #define MAX_IDS 1000
 std::atomic<int64_t> ids[MAX_IDS];
@@ -20,6 +22,19 @@ int id_add(int64_t id) {
 int id_get(int id) {
 	int64_t lid = ids[id].load(std::memory_order_relaxed);
 	return lid;
+}
+
+std::string read_from_file(std::string const& file) {
+	std::ifstream f(file);
+
+	if (f.fail()) {
+		throw std::runtime_error("Failed to open file" + file);
+	}
+
+	std::stringstream buf;
+	buf << f.rdbuf();
+
+	return buf.str();
 }
 
 TgBot::Bot bot = TgBot::Bot("");
@@ -43,23 +58,36 @@ class custom_connection : public connection {
 
 			connection::on_open(c, hdl); // calling the super function here is VERY IMPORTANT
 		}
+
+		virtual void on_fail(client* c, websocketpp::connection_hdl hdl) override {
+			connection::on_fail(c, hdl);
+			exit(-1);
+		}
 };
 
-int main() {
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		std::cout << "Usage: " << argv[0] << " [connection_file]" << std::endl;
+		return 1;
+	}
+
+	nlohmann::json connection_config = nlohmann::json::parse(read_from_file(std::string(argv[1])));
+
+	std::cout << connection_config << std::endl;
+
 	websocket_endpoint endp;
 
 	auto con = new custom_connection(endp.get_client());
-	int id = endp.connect("ws://172.24.1.87:8080", con);
+	int id = endp.connect(connection_config["url"], con);
 
-	con->authenticate("thebestbot");
-	con->log("cpp", "Hewoo");
+	con->authenticate(connection_config["key"]);
 	
 	auto sock = endp.get(id);
 
 	new(&bot) TgBot::Bot(con->config_helper.get("telegram", "token", con));
 
 	bot.getEvents().onAnyMessage([&con](TgBot::Message::Ptr message) {
-		printf("User wrote %s\n", message->text.c_str());
+		printf("User %d wrote %s\n", message->from->id, message->text.c_str());
 		if (StringTools::startsWith(message->text, "/start")) {
 			return;
 		}

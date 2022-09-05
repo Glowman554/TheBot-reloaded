c851f6b532426f025cbbad537305e1eba463b223
Glowman554 <glowman434@gmail.com>
2022-09-05 08:30:16 +0000

    added simple anime library

---
+++ libs/anime/index.ts
@@ -0,0 +1,29 @@
+import { Gogoanime } from "./provider/gogoanime.ts";
+
+export class Anime {
+    name: string;
+    episodes: number;
+    image: string;
+    url: string;
+
+    constructor(name: string, episodes: number, image: string, url: string) {
+		this.name = name;
+		this.episodes = episodes;
+		this.image = image;
+		this.url = url;
+	}
+}
+
+export interface AnimeProvider {
+    query: (q: string) => Promise<Anime[]>
+}
+
+export { Gogoanime } from "./provider/gogoanime.ts";
+
+var provider: { [key: string]: AnimeProvider } = {
+    "gogoanime": new Gogoanime()
+}
+
+export function query_by_name(p: string, q: string) {
+    return provider[p].query(q);
+}
---
+++ libs/anime/test.ts
@@ -0,0 +1,3 @@
+import { query_by_name } from "./index.ts";
+
+query_by_name("gogoanime", "my hero academia").then(console.log)
